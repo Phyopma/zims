3f5a4fddad47ad5315486470462217852b2436ee
Gregory Shklovski <gshklovs@uci.edu>
2024-02-03 00:10:47 -0800

    bug fix

--- app/components/MakerspaceDescription.tsx
+++ app/components/MakerspaceDescription.tsx
@@ -1,10 +1,10 @@
 export default function MakerspaceDescription() {
   return (
-    <div className="min-h-[75vh] w-full py-20 bg-blue-dark">
-      <div className="h-full w-2/4 mx-auto">
+    <div className="min-h-[75vh] w-full bg-blue-dark py-20">
+      <div className="mx-auto h-full w-2/4">
         <div className="chat chat-start">
-          <div className="chat-bubble chat-bubble-[red]">
-            It's over Anakin, <br />I have the high ground.
+          <div className="chat-bubble-[red] chat-bubble">
+            It&aposs over Anakin, <br />I have the high ground.
           </div>
         </div>
         <div className="chat chat-end">
